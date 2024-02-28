b5d3f9dafd4ea603bbdd6f71bf4e44202f9e9fed
yangdoyun <111186650+yangdoyun446@users.noreply.github.com>
2024-02-28 23:37:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -3,21 +3,28 @@
 
 using namespace std;
 
-int main(void){
-    int a;
-    vector<int> b(1005);
-    b[0] = 1;
-    b[1] = 1;
-    b[2] = 2;
-    cin >> a;
-    if(a == 1){
-        cout << "1" << endl;
-    }else if(a == 2){
-        cout << "2" << endl;
-    }else{
-        for(int i = 3; i <= a; i++){
-            b[i] = (b[i-1] + b[i-2]) % 10007;
-        }
-        cout << b[a] << endl;
+vector<int> v(1005);
+
+int tile(int n) {
+    if (n == 0 || n == 1){
+        return 1;
     }
+    if (v[n] > 0){
+        return v[n];
+    }
+ 
+    v[n] = (tile(n - 2) + tile(n - 1));
+    v[n] %= 10007;
+ 
+    return v[n];
+}
+
+int main() {
+    v[0] = 1;
+    v[1] = 1;
+    v[2] = 2;
+    int n;
+    cin >> n;
+    cout << tile(n);
+    return 0;
 }
