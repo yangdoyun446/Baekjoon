53e9d5d09a671b0ae9d889b8ecb483c877d56208
yangdoyun <ydy110527@namseonms.dje365.kr>
2025-02-28 18:15:35 +0900

    [Silver III] Title: 악수, Time: 68 ms, Memory: 80144 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long dp[10000001] = {0};
+int main() {
+    int n;
+    cin >> n;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10;
+    }
+
+    cout << dp[n];
+}
