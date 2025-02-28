cd8400c3cd348c6e9523b19b4505d3c1f177d543
yangdoyun <ydy110527@namseonms.dje365.kr>
2025-02-28 17:10:02 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    int dp[1000000] = {0};
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    cin >> n;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+    }
+    cout << dp[n];
+}
