a7f46640205c02d084a8283e26e5c05ebb0db928
yangdoyun <ydy110527@namseonms.dje365.kr>
2025-03-02 18:37:13 +0900

    [Silver IV] Title: 큐 2, Time: 320 ms, Memory: 18800 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int cmd, tmp;
+    string string_1;
+    queue<long long> queue_1;
+    cin >> cmd;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    for (int i = 0; i < cmd; i++) {
+        cin >> string_1;
+        if (string_1 == "push") {
+            cin >> tmp;
+            queue_1.push(tmp);
+        } else if (string_1 == "pop") {
+            if (!queue_1.empty()) {
+                cout << queue_1.front() << "\n";
+                queue_1.pop();
+            } else {
+                cout << -1 << "\n";
+            }
+        } else if (string_1 == "size") {
+            cout << queue_1.size() << "\n";
+        } else if (string_1 == "empty") {
+            cout << (queue_1.empty() ? 1 : 0) << "\n";
+        } else if (string_1 == "front") {
+            cout << (queue_1.empty() ? -1 : queue_1.front()) << "\n";
+        } else if (string_1 == "back") {
+            cout << (queue_1.empty() ? -1 : queue_1.back()) << "\n";
+        }
+    }
+}
