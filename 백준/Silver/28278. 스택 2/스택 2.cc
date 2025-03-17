b5ae185592a99d2aa0408b027bf7291c1ed3e0f1
yangdoyun <ydy110527@namseonms.dje365.kr>
2025-03-17 23:52:37 +0900

    [Silver IV] Title: 스택 2, Time: 128 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    stack<int> S;
+
+    int order;
+    while(N--){
+        cin >> order;
+        switch(order){
+            case 1:
+                int num;
+                cin>>num;
+                S.push(num);
+                break;
+            case 2:
+                if(!S.empty()){
+                    cout<<S.top()<<'\n';
+                    S.pop();
+                }else{
+                    cout<<-1<<'\n';
+                }
+                break;
+            case 3:
+                cout<<S.size()<<'\n';
+                break;
+            case 4:
+                if(!S.empty()){
+                    cout<<0<<'\n';
+                }else{
+                    cout<<1<<'\n';
+                }
+                break;
+            case 5:
+                if(!S.empty()){
+                    cout<<S.top()<<'\n';
+                }else{
+                    cout<<-1<<'\n';
+                }
+                break;
+        }
+    }
+    return 0;
+}
